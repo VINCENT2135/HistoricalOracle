aca2d7413d19075c1dc5fe324f92c3708dbe58e6
Vincent Ivos <vincentivos1@gmail.com>
2021-03-30 11:48:48 -0400

    article controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
-  
+  root "pages#home"
+  get "about", to: "pages#about"
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, destroy]
+  #or
+  resources :articles
+  get "signup", to: "users#new"
+
+  #post 'users', to: 'users#create' this was needed to avoid that path error
+  resources :users, except: [:new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :categories, except: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
