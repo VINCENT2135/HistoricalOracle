381c79e5bc0450ac65816eaf6e732a4badbb95b8
VINCENT2135 <68698502+VINCENT2135@users.noreply.github.com>
2021-03-30 16:19:59 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
+        flash[:notice] = "Welcome to Historical Oracle #{@user.username}."
         redirect_to articles_path
       else
         render 'new'
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     def destroy
       @user.destroy
       session[:user_id] = nil if @user == current_user
-      flash[:notice] = "Account and all associated articles successfully deleted"
+      flash[:notice] = "Account and all articles have been deleted"
       redirect_to articles_path
     end
   
