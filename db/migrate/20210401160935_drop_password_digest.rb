77b93b2e8515427989875cadd7fad316ab8dba3c
Vincent Ivos <vincentivos1@gmail.com>
2021-04-01 15:04:55 -0400

    db mod

--- db/migrate/20210401160935_drop_password_digest.rb
+++
@@ -1,7 +0,0 @@
-class DropPasswordDigest < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :add_password_digest
-  end
-end
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_205718) do
+ActiveRecord::Schema.define(version: 2021_03_30_191328) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
     t.integer "category_id"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.string "description"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -36,10 +37,10 @@ ActiveRecord::Schema.define(version: 2021_03_31_205718) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
-    t.string "password_digest"
   end
 
 end
