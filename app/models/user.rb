289e55c1626ff794435d9c588c0724b2449216ee
VINCENT2135 <68698502+VINCENT2135@users.noreply.github.com>
2021-04-05 14:44:43 -0400

    Omniauth bug fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
 
                 User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |u|
                         u.username = response[:info][:name]
-                        u.email = response [:info][:name]
+                        u.email = response [:info][:email]
                         u.password = SecureRandom.hex(15)
 
                 end
